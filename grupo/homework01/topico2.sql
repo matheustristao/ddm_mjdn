909a6471732910934f1cfa11f6a9fc02b107d48a
Ziegired <38291582+Ziegired@users.noreply.github.com>
2024-11-09 16:09:35 +0000

    exerc50 done

--- grupo/homework01/topico2.sql
+++ grupo/homework01/topico2.sql
@@ -1,6 +1,6 @@
--- SQL - Exercícios 2
+-- SQL - Exercícios II
 
--- 1 Para cada Cliente (CompanyName), listar os diferentes Endereços identificando o Tipo de Endereço
+--Exerc. 1 - Para cada Cliente (CompanyName), listar os diferentes Endereços identificando o Tipo de Endereço
 select distinct cus.CompanyName, ca.AddressType,ad.AddressLine1, ad.AddressLine2, ad.City, ad.StateProvince, ad.CountryRegion, ad.PostalCode
     from Customer as cus
     join CustomerAddress as ca
@@ -9,9 +9,18 @@ select distinct cus.CompanyName, ca.AddressType,ad.AddressLine1, ad.AddressLine2
         on ca.AddressID = ad.AddressID   
     order by cus.CompanyName     
 
---4 Listar os clientes (CustomerID e CompanyName) sem encomendas associadas (SalesOrderHeader)
+--Exerc. 4 - Listar os clientes (CustomerID e CompanyName) sem encomendas associadas (SalesOrderHeader)
 select cus.CustomerID, cus.CompanyName, soh.CustomerID
     FROM Customer cus
     left join SalesOrderHeader soh
         on cus.CustomerID=soh.CustomerID 
         where soh.CustomerID is null
+
+
+/* Exerc. 50 - Comando select para listar: idpeca-filho, nomepeça-filho, idpeca-pai,
+nomepeça-pai, quantidade do filho para fabricar o pai. A peça A que é o topo da estrutura (produto
+final) também deve ser listada. */
+
+select pnf.idpeca "idpeca-filho", pnf.nomepeca "nomepeça-filho", pnp.idpeca "idpeca-pai", pnp.nomepeca "nomepeça-pai", pnf.qtd "quantidade-filho" 
+    from pecanew pnf left join pecanew pnp 
+    on pnf.idpai = pnp.idpeca
