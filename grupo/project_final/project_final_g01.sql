f48990ba71e1661bb650855cc2f5f1b96a0b9478
matheustristao <matheus.tristao@gmail.com>
2024-11-14 18:06:58 +0000

    projecto ex 1

---
+++ grupo/project_final/project_final_g01.sql
@@ -0,0 +1,14 @@
+/* 
+    Matheus dos Anjos
+    Nuno
+    Diogo
+    Jesus
+
+    Data Management
+*/
+
+-- 1. Listar os títulos de todos os filmes dirigidos por Steven Spielberg.
+
+select titulo
+    from Filme 
+    where realizador = 'Steven Spielberg';
