610bcfe4e02bb88a67b9a8df1c40c98c9c6ec36b
Ziegired <38291582+Ziegired@users.noreply.github.com>
2024-11-15 23:52:41 +0000

    Homework02 exerc19 e altercaao noutros exerc.

--- grupo/homework02/Homework02_grupo01.sql
+++ grupo/homework02/Homework02_grupo01.sql
@@ -1,25 +1,26 @@
+-- Homework 02:
+
 -- SQL Exercícios II
 
--- 39 Escreva um comando sql que liste idpeca-filho, nomepeca-filho, idpeca-pai, nomepeca-pai,
---    quantidade do filho que é necessária para o fabrico do pai. A Peça A (Produto Final) que não
---    tem pai também deve aparecer na listagem.
+/* Exerc. 39 - Escreva um comando sql que liste idpeca-filho, nomepeca-filho, idpeca-pai, nomepeca-pai, quantidade
+do filho que é necessária para o fabrico do pai. A Peça A (Produto Final) que não tem pai também deve aparecer na 
+listagem. */
 
-select h.idfilho "idpeca-filho",
+select p_filho.idpeca "idpeca-filho",
        p_filho.nomepeca "nomepeca-filho", 
-       p.idpeca "idpeca-pai", 
-       p.nomepeca "nomepeca-pai", 
-       h.qtd
-    from peca p 
+       p_pai.idpeca "idpeca-pai", 
+       p_pai.nomepeca "nomepeca-pai", 
+       h.qtd "quantidade filho"
+    from peca p_filho 
     left join hch h
-        on p.idpeca = h.idpai
-    left join peca p_filho
-        on h.idfilho = p_filho.idpeca;
+        on p_filho.idpeca = h.idfilho
+    left join peca p_pai
+        on h.idpai = p_pai.idpeca;
 
--- 52. Liste o ProductNumber, Name e média do preço de venda dos Produtos em que essa média é
---     superior à média do preço de venda de todos os produtos. Ordene por média do preço de venda
---     descendente.              
+/* Exerc. 52 - Liste o ProductNumber, Name e média do preço de venda dos Produtos em que essa média é
+superior à média do preço de venda de todos os produtos. Ordene por média do preço de venda descendente. */
 
-select ProductNumber, Name, avg(ListPrice) avg_listPrice
+select ProductNumber, Name, avg(ListPrice) "média do preço de venda"
     from Product p
     group by ProductNumber, Name
     having avg(ListPrice) > (
@@ -28,23 +29,53 @@ select ProductNumber, Name, avg(ListPrice) avg_listPrice
     )
     order by avg(ListPrice) desc;
 
--- 19. 
+/* Exerc. 19 - Liste a média das vendas em valor para o par (modelo, produto), para os casos em que essa
+média é superior à média das vendas do respetivo modelo */
 
+select p1.ProductModelID, p1.ProductID, avg(sod1.LineTotal) "Média das vendas"
+    from product p1
+    join SalesOrderDetail sod1
+        on p1.ProductID = sod1.ProductID
+    group by p1.ProductModelID, p1.ProductID
+    having avg(sod1.LineTotal) >
+        (select avg(sod2.LineTotal)
+            from SalesOrderDetail sod2
+            join product p2
+                on sod2.ProductID = p2.ProductID
+            where p2.ProductModelID = p1.ProductModelID);
 
--- 23. Liste o Código das categorias, o código do modelo, o código dos produtos e o somatório de
--- vendas por: código de categoria, (código de categoria, código do modelo),
--- (código de categoria, código do modelo, código de produto) e total geral    
+/* Exerc. 23 - Liste o Código das categorias, o código do modelo, o código dos produtos e o somatório de
+vendas por: código de categoria, (código de categoria, código do modelo),
+(código de categoria, código do modelo, código de produto) e total geral */
 
-select ProductCategoryID, ProductModelID, ProductNumber, sum(ListPrice) sum_ListPrice
-    from Product
-    group by rollup(ProductCategoryID, ProductModelID, ProductNumber);
+select p.ProductCategoryID, p.ProductModelID, p.ProductID, sum(LineTotal) "Somatório de Vendas"
+    from Product p join SalesOrderDetail sod
+    on p.productID = sod.ProductID
+    group by rollup(p.ProductCategoryID, p.ProductModelID, p.ProductID);
+
+-- Usando o Grouping Sets
+
+select p.ProductCategoryID, p.ProductModelID, p.ProductID, sum(LineTotal) "Somatório de Vendas"
+    from Product p join SalesOrderDetail sod
+    on p.productID = sod.ProductID
+    group by Grouping sets (p.ProductCategoryID, (p.ProductCategoryID, p.ProductModelID),
+        (p.ProductCategoryID, p.ProductModelID, p.ProductID), ());
                       
--- 24. Liste o Código das categorias, o código do modelo, o código dos produtos e o somatório de
--- vendas por: código de categoria, código de modelo, código de produto, (código de categoria,
--- código do modelo), (código de categoria, código de produto), (código de modelo, código de produto) e
--- (código de categoria, código do modelo, código de produto) e total geral
+/* Exerc 24 - Liste o Código das categorias, o código do modelo, o código dos produtos e o somatório de
+vendas por: código de categoria, código de modelo, código de produto, (código de categoria,
+código do modelo), (código de categoria, código de produto), (código de modelo, código de produto) e
+(código de categoria, código do modelo, código de produto) e total geral */
 
-select ProductCategoryID, ProductModelID, ProductNumber, sum(ListPrice) sum_ListPrice
-    from Product
-    group by cube(ProductCategoryID, ProductModelID, ProductNumber);
-                      
+select p.ProductCategoryID, p.ProductModelID, p.ProductID, sum(LineTotal) "Somatório de Vendas"
+    from Product p join SalesOrderDetail sod
+    on p.productID = sod.ProductID
+    group by cube(p.ProductCategoryID, p.ProductModelID, p.ProductID);
+
+-- Usando o Grouping Sets
+
+select p.ProductCategoryID, p.ProductModelID, p.ProductID, sum(LineTotal) "Somatório de Vendas"
+    from Product p join SalesOrderDetail sod
+    on p.productID = sod.ProductID
+    group by grouping sets (p.ProductCategoryID, p.ProductModelID, p.ProductID, (p.ProductCategoryID, p.ProductModelID)
+    , (p.ProductCategoryID, p.ProductID), (p.ProductModelID, p.ProductID)
+    , (p.ProductCategoryID, p.ProductModelID, p.ProductID),());
