a7470a1ccf8bc755a2b9e3af7190c49a5df0c04a
matheustristao <matheus.tristao@gmail.com>
2024-11-22 23:42:32 +0000

    54 using with

--- grupo/homework03/Homework03_grupo01_variasopcoes.sql
+++ grupo/homework03/Homework03_grupo01_variasopcoes.sql
@@ -57,21 +57,39 @@ group by CountryRegion;
 Para cada categoria (nome) liste o número de produtos dessa categoria, bem como a função de
 distribuição acumulada relativa a esse número de produtos, arredondada a duas casas decimais.*/
 
-select pc.Name, count(p.ProductID) 'numero de produtos',
-round(cume_dist() over (order by count(p.ProductID)),2) 'distribuição'
+select pc.Name, count(p.ProductID) 'NumberProducts',
+round(cume_dist() over (order by count(p.ProductID)),2) distribuicao
 from Product p
 join ProductCategory pc on p.ProductcategoryID = pc.ProductCategoryID
 group by pc.name
-order by distribuição desc
+order by distribuicao desc
 
 /*Exercio II - Exerc. 54
 Para cada categoria (nome) liste os produtos dessa categoria (name), a soma das vendas
 (linetotal) desses produtos, bem como a função de distribuição acumulada relativa a essa soma,
 arredondada a duas casas decimais.*/
 
-select pc.name, p.Name, sum(linetotal),
-round(cume_dist() over (partition by pc.name order by sum(linetotal)),2) 'distribuição'
+select pc.name categoryName, p.Name productName, sum(linetotal) as sum_line_total,
+round(cume_dist() over (partition by pc.name order by sum(linetotal)),2) distribuicao
 from Product p
 join ProductCategory pc on p.ProductcategoryID = pc.ProductCategoryID
 join SalesOrderDetail sod on p.ProductID = sod.ProductID
 group by pc.name, p.Name
+order by 1 asc, 4 desc;
+
+
+-- Using WITH
+with sum_cte as (
+    select pc.name categoryName, p.Name productName, sum(linetotal) sum_line_total
+        from Product p
+        join ProductCategory pc 
+            on p.ProductcategoryID = pc.ProductCategoryID
+        join SalesOrderDetail sod  
+            on p.ProductID = sod.ProductID
+        group by pc.name, p.Name 
+)
+
+select categoryName, productName, sum_line_total,
+       round(cume_dist() over (partition by categoryName order by sum_line_total),2) distribuicao
+from sum_cte
+order by 1 asc, 4 desc;
