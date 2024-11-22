974eadb458accfb39129c0ce924be26092955ca2
Ziegired <38291582+Ziegired@users.noreply.github.com>
2024-11-22 22:57:29 +0000

    exerc 12 - com varias opcoes

--- grupo/homework03/Homework03_grupo01_variasopcoes.sql
+++ grupo/homework03/Homework03_grupo01_variasopcoes.sql
@@ -15,6 +15,7 @@ where rank_totaldue = 1
 Por CountryRegion liste o número de Clientes que fizeram encomendas cuja soma é superior a
 $50000, bem como esse somatório por CountryRegion*/
 
+--Using only aggregations
 select CountryRegion, count(*) 'numero de clientes', sum(sum_totaldue) 'soma de vendas por countryregion'
 from
 (select CountryRegion, c.customerid, sum(TotalDue) sum_totaldue
@@ -26,7 +27,7 @@ group by CountryRegion, c.CustomerID
 having sum(TotalDue) > 50000) x
 group by CountryRegion;
 
---WITH
+--Using WITH
 with cte_SumTotalDue AS
 (select CountryRegion, c.customerid, sum(TotalDue) sum_totaldue
 from Customer c
@@ -40,7 +41,7 @@ select CountryRegion, count(*) 'numero de clientes', sum(sum_totaldue) 'soma de 
 from cte_SumTotalDue
 group by CountryRegion;
 
---WINDOWS FUNCTION
+--using WINDOWS FUNCTION
 select countryregion, count(*), sum(sum_totaldue)
 from 
 (select countryregion, c.customerid,
@@ -51,4 +52,8 @@ join Address a on ca.AddressID = a.AddressID
 join SalesOrderHeader sod on c.CustomerID = sod.CustomerID) x
 where sum_totaldue > 50000
 group by CountryRegion;
+
+/*Exercico II - Exerc. 48
+Para cada categoria (nome) liste o número de produtos dessa categoria, bem como a função de
+distribuição acumulada relativa a esse número de produtos, arredondada a duas casas decimais.*/
 
