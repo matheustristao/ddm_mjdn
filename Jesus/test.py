5f581f71a7f3e4da111b5f4371d461771d28f1c2
Anzola10 <jesusanzola92@gmail.com>
2025-06-03 18:15:52 +0100

    Test

---
+++ Jesus/test.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import openpyxl
+import xlrd
+import xlwt
+import xlsxwriter
+import csv
+import json
+import requests
+import time
+import datetim    
+import pyodbc
+
+def OpenExcel(excelFile): 
